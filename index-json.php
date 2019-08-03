9549a5ceb41357901a49d1daa688075c0bda579d
matt <mattwaldron@ymail.com>
2019-08-02 21:17:54 -0400

    labeling and delete toggle

--- index-json.php
+++ index-json.php
@@ -1,66 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Bare - Start Bootstrap Template</title>
-	
-	<!-- FullCalendar -->
-	<link href='css/fullcalendar.min.css' rel='stylesheet' />
-    <!-- Bootstrap Core CSS -->
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'></script>
-	<script src='js/moment.min.js'></script>
-	<!-- jQuery Version 1.9.1 -->
-    <script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ=" crossorigin="anonymous"></script>
-	<!-- FullCalendar -->
-	<script src='js/fullcalendar.min.js'></script>
-	 <!-- Bootstrap Core JavaScript -->
-    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Bare - Start Bootstrap Template</title>
 
-    <!-- Custom CSS -->
-	<style>
-        #calendar {
-            max-width: 1200px;
-            margin-bottom: 30px;
-        }
-        .nocheckbox {
-            display: none;
-        }
-        .label-on {
-            border-radius: 3px;
-            background: red;
-            color: #ffffff;
-            padding: 6px 10px;
-            border: 1px solid red;
-            display: table-cell;
-        }
-        .label-off {
-            border-radius: 3px;
-            background: white;
-            border: 1px solid red;
-            padding: 6px 10px;
-            display: table-cell;
-        }
+<!-- FullCalendar -->
+<link href='css/fullcalendar.min.css' rel='stylesheet' />
+<!-- Bootstrap Core CSS -->
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'></script>
+<script src='js/moment.min.js'></script>
+<!-- jQuery Version 1.9.1 -->
+<script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ=" crossorigin="anonymous"></script>
+<!-- FullCalendar -->
+<script src='js/fullcalendar.min.js'></script>
+	<!-- Bootstrap Core JavaScript -->
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
 
-        #recurrence-form {
-            display: none;
-        }
-        #calendar a.fc-event {
-            color: #fff; /* bootstrap default styles make it black. undo */
-            background-color: #0065A6;
-        }
-    </style>
+<!-- Custom CSS -->
+<style>
+#calendar {
+	max-width: 1200px;
+	margin-bottom: 30px;
+}
+.nocheckbox {
+	display: none;
+}
+.label-on {
+	border-radius: 3px;
+	background: red;
+	color: #ffffff;
+	padding: 6px 10px;
+	border: 1px solid red;
+	display: table-cell;
+}
+.label-off {
+	border-radius: 3px;
+	background: white;
+	border: 1px solid red;
+	padding: 6px 10px;
+	display: table-cell;
+}
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+#recurrence-form {
+	display: none;
+}
+#calendar a.fc-event {
+	color: #fff; /* bootstrap default styles make it black. undo */
+	background-color: #0065A6;
+}
+</style>
+
 </head>
 <body>
 <!-- Page Content -->
@@ -74,7 +68,7 @@
 		</div>
 	</div>
 
-	<!-- Modal -->
+	<!-- Add Modal -->
 	<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
@@ -158,26 +152,26 @@
 							</div>
 							<div class="row">
 								<div class="form-group">
+									<label class="col-md-1 checkbox-inline" for="checkboxes-6">
+										<input type="checkbox" name="dowID[]" id="checkboxes-6" value="6">Su
+									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-0">
-										<input type="checkbox" name="dowID[]" id="checkboxes-0" value="6">Su
+										<input type="checkbox" name="dowID[]" id="checkboxes-0" value="0">Mo
 									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-1">
-										<input type="checkbox" name="dowID[]" id="checkboxes-1" value="0">Mo
+										<input type="checkbox" name="dowID[]" id="checkboxes-1" value="1">Tu
 									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-2">
-										<input type="checkbox" name="dowID[]" id="checkboxes-2" value="1">Tu
+										<input type="checkbox" name="dowID[]" id="checkboxes-2" value="2">We
 									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-3">
-										<input type="checkbox" name="dowID[]" id="checkboxes-3" value="2">We
+										<input type="checkbox" name="dowID[]" id="checkboxes-3" value="3">Th
 									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-4">
-										<input type="checkbox" name="dowID[]" id="checkboxes-4" value="3">Th
+										<input type="checkbox" name="dowID[]" id="checkboxes-4" value="4">Fr
 									</label>
 									<label class="col-md-1 checkbox-inline" for="checkboxes-5">
-										<input type="checkbox" name="dowID[]" id="checkboxes-5" value="4">Fr
-									</label>
-									<label class="col-md-1 checkbox-inline" for="checkboxes-6">
-										<input type="checkbox" name="dowID[]" id="checkboxes-6" value="5">Sa
+										<input type="checkbox" name="dowID[]" id="checkboxes-5" value="5">Sa
 									</label>
 								</div>
 							</div>
@@ -207,7 +201,7 @@
 		</div>
 	</div>
 		
-	<!-- Modal -->
+	<!-- Edit Modal -->
 	<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
@@ -260,25 +254,14 @@
 								<input type="text" name="description" class="form-control" id="description" placeholder="Description">
 							</div>
 						</div>
-						<div class="form-group" id="del"> 
+						<div class="form-group"> 
 							<label class="col-sm-12 control-label">Delete Event</label>
 							<div class="col-sm-12">
-								<label onclick="toggleCheck('check1');" class="label-off" for="check1" id="check1_label">Delete</label>
+								<label class="delete label-off" for="del" id="delete">Delete
+								<input class="nocheckbox" type="checkbox" id="del" name="delete" value="delete"></label>
 							</div>
-							<input class="nocheckbox" type="checkbox" id="check1" name="delete">
 						</div>
 					</div>
-					<script>
-					function toggleCheck(check) {
-						if ($('#'+check).is(':checked')) {
-							$('#'+check+'_label').removeClass('label-on');
-							$('#'+check+'_label').addClass('label-off');
-						} else {
-							$('#'+check+'_label').addClass('label-on');
-							$('#'+check+'_label').removeClass('label-off');
-						}
-					}		  
-					</script>
 				</div>
 				<div id="editRecurrence">
 				<div class="modal-header">
@@ -295,9 +278,9 @@
 						<div class="form-group"> 
 							<label class="col-sm-12 control-label">Delete Recurrence</label>
 							<div class="col-sm-12">
-								<label onclick="toggleCheck('check2');" class="label-off" for="check2" id="check2_label">Delete</label>
+								<label class="deleteRecurrence label-off" for="delRec" id="deleteRecurrence">Delete
+								<input class="nocheckbox" type="checkbox" id="delRec" name="deleteRecurrence" value="deleteRecurrence"></label>
 							</div>
-							<input class="nocheckbox" type="checkbox" id="check2" name="deleteRecurrence">
 						</div>
 					</div>
 					<input type="hidden" name="id" class="form-control" id="id">
@@ -315,107 +298,128 @@
 
 </div>
 
+<script>		
+$("#del, #delRec").change(function() {	
+	var self = $(this);
+	var aValue = self.attr("value");
+	var aClass = self.attr("class");
+	var checked = $("input:checkbox[class='" + aClass + "']");
+
+	if ( $(self).is(':checked') ) {
+		$("." + aValue).removeClass("label-off").addClass('label-on');
+		$(checked).not(self).removeAttr("checked");
+		if ( aValue == 'deleteRecurrence' ) {
+			$('#delete').removeClass("label-on").addClass('label-off');
+		} else {
+			$('#deleteRecurrence').removeClass("label-on").addClass('label-off');
+		}
+	} else {
+		$("." + aValue).removeClass("label-on").addClass('label-off');
+	}
+});
+</script>
+
 <script>
-	$(function() {
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next, today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay, listWeek'
-			},
-			height: 540,
-			businessHours: {
-			  dow: [ 1, 2, 3, 4, 5 ],
-			  start: '8:00',
-			  end: '17:00',
-			},
-			nowIndicator: true,
-			now: '2019-01-11T11:15:00', //remove - only for demo
-			scrollTime: '08:00:00',
-			defaultDate: '2019-01-07', // Use this line to use the current date: new Date(),
-			editable: true,
-			navLinks: true,
-			eventLimit: true, // allow "more" link when there are too many events
-			selectable: true,
-			selectHelper: true,
-			select: function(start, end) {
-				$('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
-				$('#ModalAdd #startDate').val(moment(start).format('YYYY-MM-DD'));
-				$('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
-				$('#ModalAdd #endDate').val(moment(start).add(2, 'w').format('YYYY-MM-DD'));
-				$('#ModalAdd').modal('show');
-			},
-			eventAfterRender: function(eventObj, $el) {
-				var request = new XMLHttpRequest();
-				request.open('GET', 'json/events.json', true);
-				request.onload = function () {
-					$el.popover({
-						title: eventObj.title,
-						content: eventObj.description,
-						trigger: 'hover',
-						placement: 'top',
-						container: 'body'
-					});
-				}
-			request.send();
-			},
-			eventRender: function(event, element) {
-				element.bind('dblclick', function() {
-					$('#ModalEdit #id').val(event.id);
-					$('#ModalEdit #rid').val(event.rid);
-					$('#ModalEdit #start').val(moment(event.start).format('YYYY-MM-DD HH:mm:ss'));
-					$('#ModalEdit #end').val(moment(event.end).format('YYYY-MM-DD HH:mm:ss'));
-					$('#ModalEdit #title').val(event.title);
-					$('#ModalEdit #description').val(event.description);
-					$('#ModalEdit #color').val(event.color);
-					$('#ModalEdit #rstatus').val(event.recurrence);
-					if (event.recurrence === 'true') {
-                        $('#editRecurrence').show();
-					} else {
-                        $('#editRecurrence').hide();
-					}
-					$('#ModalEdit').modal('show');
+$(function() {
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next, today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay, listWeek'
+		},
+		height: 540,
+		businessHours: {
+			dow: [ 1, 2, 3, 4, 5 ],
+			start: '8:00',
+			end: '17:00',
+		},
+		nowIndicator: true,
+		now: '2019-01-11T11:15:00', //remove - only for demo
+		scrollTime: '08:00:00',
+		defaultDate: '2019-01-07', // Use this line to use the current date: new Date(),
+		editable: true,
+		navLinks: true,
+		eventLimit: true, // allow "more" link when there are too many events
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end) {
+			$('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
+			$('#ModalAdd #startDate').val(moment(start).format('YYYY-MM-DD'));
+			$('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
+			$('#ModalAdd #endDate').val(moment(start).add(2, 'w').format('YYYY-MM-DD'));
+			$('#ModalAdd').modal('show');
+		},
+		eventAfterRender: function(eventObj, $el) {
+			var request = new XMLHttpRequest();
+			request.open('GET', 'json/events.json', true);
+			request.onload = function () {
+				$el.popover({
+					title: eventObj.title,
+					content: eventObj.description,
+					trigger: 'hover',
+					placement: 'top',
+					container: 'body'
 				});
-			},
-			eventDrop: function(event, delta, revertFunc) { // si changement de position
-				edit(event);
-			},
-			eventResize: function(event,dayDelta,minuteDelta,revertFunc) { // si changement de longueur
-				edit(event);
-			},
-            events: {
-                url: 'php/get-events.php',
-                error: function() {
-                $('#script-warning').show();
-                }
-            },
-            loading: function(bool) {
-                $('#loading').toggle(bool);
-            }
-		});
-		function edit(event) {
-			start = event.start.format('YYYY-MM-DD HH:mm:ss');
-			if (event.end) {
-				end = event.end.format('YYYY-MM-DD HH:mm:ss');
-			} else {
-				end = start;
 			}
-			id = event.id;
-			Event = [];
-			Event[0] = id;
-			Event[1] = start;
-			Event[2] = end;
-			
-			$.ajax({
-			 url: 'editEventDate-json.php',
-			 type: "POST",
-			 data: {Event:Event},
-			 success: function(rep) {
-				 alert('Saved');
+		request.send();
+		},
+		eventRender: function(event, element) {
+			element.bind('click', function() {
+				$('#ModalEdit #id').val(event.id);
+				$('#ModalEdit #rid').val(event.rid);
+				$('#ModalEdit #start').val(moment(event.start).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalEdit #end').val(moment(event.end).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalEdit #title').val(event.title);
+				$('#ModalEdit #description').val(event.description);
+				$('#ModalEdit #color').val(event.color);
+				$('#ModalEdit #rstatus').val(event.recurrence);
+				if (event.recurrence === 'true') {
+					$('#editRecurrence').show();
+				} else {
+					$('#editRecurrence').hide();
 				}
+				$('#ModalEdit').modal('show');
 			});
+		},
+		eventDrop: function(event, delta, revertFunc) { // si changement de position
+			edit(event);
+		},
+		eventResize: function(event,dayDelta,minuteDelta,revertFunc) { // si changement de longueur
+			edit(event);
+		},
+		events: {
+			url: 'php/get-events.php',
+			error: function() {
+			$('#script-warning').show();
+			}
+		},
+		loading: function(bool) {
+			$('#loading').toggle(bool);
 		}
 	});
+	function edit(event) {
+		start = event.start.format('YYYY-MM-DD HH:mm:ss');
+		if (event.end) {
+			end = event.end.format('YYYY-MM-DD HH:mm:ss');
+		} else {
+			end = start;
+		}
+		id = event.id;
+		Event = [];
+		Event[0] = id;
+		Event[1] = start;
+		Event[2] = end;
+		
+		$.ajax({
+			url: 'editEventDate-json.php',
+			type: "POST",
+			data: {Event:Event},
+			success: function(rep) {
+				alert('Saved');
+			}
+		});
+	}
+});
 </script>
 
 </body>
